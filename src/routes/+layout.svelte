0963d6ecccaff3e647d127c24b93fc7de0d5e2d6
Matayay Karuna <matayay.karuna@colorado.edu>
2023-06-30 00:07:23 -0600

    created footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
 </script>
 
 <Header />
 <slot />
+<Footer />
---
+++ src/routes/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	const time = new Date();
+</script>
+
+<footer
+	class="flex h-20 items-center justify-center border-x-0 border-t-[0.1px] border-gray-500 bg-black"
+>
+	<h3 class="text-white">© {time.getFullYear()} Matayay Karuna</h3>
+</footer>
