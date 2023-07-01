d3efb39da75cc014d3966c79e18ecb7faea6dc50
Matayay Karuna <matayay.karuna@colorado.edu>
2023-06-30 22:15:12 -0600

    implemented animated sidebar

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -2,17 +2,20 @@
 	import GitHub from './GitHub.svelte';
 	import Linkedin from './Linkedin.svelte';
 	import Instagram from './Instagram.svelte';
+	import SideBar from './SideBar.svelte';
+
+	let showSidebar = false;
 </script>
 
 <div class="sticky left-0 top-0">
 	<header class="flex h-20 items-center justify-end bg-gradient-to-b from-sky-950 px-6">
 		<div class="hidden min-w-full items-center justify-around sm:flex">
 			<nav class="flex items-center justify-center gap-4 lg:gap-10">
-				<h3 class="cursor-pointer text-white">Home</h3>
-				<h3 class="cursor-pointer text-white">About</h3>
-				<h3 class="cursor-pointer text-white">Experience</h3>
-				<h3 class="cursor-pointer text-white">Projects</h3>
-				<h3 class="cursor-pointer text-white">Skills</h3>
+				<button class="cursor-pointer text-white">Home</button>
+				<button class="cursor-pointer text-white">About</button>
+				<button class="cursor-pointer text-white">Experience</button>
+				<button class="cursor-pointer text-white">Projects</button>
+				<button class="cursor-pointer text-white">Skills</button>
 			</nav>
 
 			<nav class="flex items-center justify-center gap-2 lg:gap-8">
@@ -22,12 +25,16 @@
 			</nav>
 		</div>
 
-		<button class="flex min-h-full cursor-pointer flex-col justify-center gap-2 sm:hidden">
-			<div class="h-1 w-10 bg-gray-300" />
-			<div class="h-1 w-10 bg-gray-300" />
-			<div class="h-1 w-10 bg-gray-300" />
+		<button
+			class="flex min-h-full cursor-pointer flex-col justify-center gap-2 sm:hidden"
+			on:click={() => (showSidebar = true)}
+		>
+			<div class="h-1 w-10 rounded-xl bg-gray-500" />
+			<div class="h-1 w-10 rounded-xl bg-gray-500" />
+			<div class="h-1 w-10 rounded-xl bg-gray-500" />
 		</button>
 	</header>
+	<SideBar bind:showSidebar />
 </div>
 
 <!-- from-[rgba(0,26,73,0.99)] -->
---
+++ src/routes/components/SideBar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let showSidebar: boolean;
+	import XIcon from './XIcon.svelte';
+	import GitHub from './GitHub.svelte';
+	import Linkedin from './Linkedin.svelte';
+	import Instagram from './Instagram.svelte';
+</script>
+
+<div
+	class={`fixed right-0 top-0 flex min-h-screen w-3/4 flex-col gap-8 bg-sky-200 duration-500 ease-in-out ${
+		showSidebar ? 'translate-x-0' : 'translate-x-full'
+	}`}
+>
+	<div class="flex w-full items-center justify-end p-4">
+		<button class="text-6xl text-gray-500" on:click={() => (showSidebar = false)}>
+			<XIcon />
+		</button>
+	</div>
+	<div class="flex flex-col items-center justify-center gap-8">
+		<nav class="flex flex-col items-end justify-center gap-8">
+			<button class="w-40 rounded-xl bg-amber-100 py-2 text-center text-black">Home</button>
+			<button class="w-40 rounded-xl bg-amber-100 py-2 text-center text-black">About</button>
+			<button class="w-40 rounded-xl bg-amber-100 py-2 text-center text-black">Experience</button>
+			<button class="w-40 rounded-xl bg-amber-100 py-2 text-center text-black">Projects</button>
+			<button class="w-40 rounded-xl bg-amber-100 py-2 text-center text-black">Skills</button>
+		</nav>
+		<nav class="flex items-center justify-center gap-4">
+			<GitHub />
+			<Linkedin />
+			<Instagram />
+		</nav>
+	</div>
+</div>
