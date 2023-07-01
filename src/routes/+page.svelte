10008e2f7cb9afded6d311de461775097745eca2
Matayay Karuna <matayay.karuna@colorado.edu>
2023-06-30 23:37:29 -0600

    installed threlte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core';
+	import { spring } from 'svelte/motion';
+	import { degToRad } from 'three/src/math/MathUtils';
+
+	const scale = spring(1);
 </script>
 
 <div class="min-h-screen bg-black" />
