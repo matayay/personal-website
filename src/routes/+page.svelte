a026dbd537731591eee800b64c6817232c6d936c
Matayay Karuna <matayay.karuna@colorado.edu>
2023-07-01 03:03:50 -0600

    made home section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,39 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	let transition = false;
+	onMount(() => {
+		transition = true;
+	});
 </script>
 
-<div class="min-h-screen bg-black" />
+<div class="min-h-screen bg-black">
+	<div class="flex h-[75vh] items-center justify-center">
+		<div class="flex flex-col gap-4 sm:gap-8">
+			{#if transition}
+				<div class="flex gap-4">
+					<h2
+						in:fade={{ delay: 750, duration: 500 }}
+						class="font-mono text-xl text-neutral-300 sm:text-4xl lg:text-6xl"
+					>
+						Hello There,
+					</h2>
+					<h2
+						in:fade={{ delay: 1500, duration: 500 }}
+						class="font-mono text-xl text-neutral-300 sm:text-4xl lg:text-6xl"
+					>
+						I'm
+					</h2>
+				</div>
+
+				<h1
+					in:fade={{ delay: 2500, duration: 500 }}
+					class="font-sans text-4xl text-white sm:text-7xl lg:text-9xl"
+				>
+					Matayay Karuna
+				</h1>
+			{/if}
+		</div>
+	</div>
+</div>
--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -3,35 +3,47 @@
 	import Linkedin from './Linkedin.svelte';
 	import Instagram from './Instagram.svelte';
 	import SideBar from './SideBar.svelte';
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
 	let showSidebar = false;
+	let transition = false;
+	onMount(() => {
+		transition = true;
+	});
 </script>
 
 <div class="sticky left-0 top-0">
-	<header class="flex h-20 items-center justify-end bg-gradient-to-b from-sky-950 px-6">
-		<div class="hidden min-w-full items-center justify-around sm:flex">
-			<nav class="flex items-center justify-center gap-4 lg:gap-10">
-				<button class="cursor-pointer text-white">Home</button>
-				<button class="cursor-pointer text-white">About</button>
-				<button class="cursor-pointer text-white">Experience</button>
-				<button class="cursor-pointer text-white">Projects</button>
-				<button class="cursor-pointer text-white">Skills</button>
-			</nav>
-
-			<nav class="flex items-center justify-center gap-2 lg:gap-8">
-				<GitHub />
-				<Linkedin />
-				<Instagram />
-			</nav>
-		</div>
-
-		<button
-			class="flex min-h-full cursor-pointer flex-col justify-center gap-2 sm:hidden"
-			on:click={() => (showSidebar = true)}
+	{#if transition}
+		<header
+			in:fade={{ delay: 3500, duration: 1500 }}
+			class="flex h-20 items-center justify-end bg-gradient-to-b from-sky-950 px-6"
 		>
-			<div class="h-1 w-10 rounded-xl bg-gray-500" />
-			<div class="h-1 w-10 rounded-xl bg-gray-500" />
-			<div class="h-1 w-10 rounded-xl bg-gray-500" />
-		</button>
-	</header>
-	<SideBar bind:showSidebar />
+			<div class="hidden min-w-full items-center justify-around sm:flex">
+				<nav class="flex items-center justify-center gap-4 lg:gap-10">
+					<button class="cursor-pointer text-white">Home</button>
+					<button class="cursor-pointer text-white">About</button>
+					<button class="cursor-pointer text-white">Experience</button>
+					<button class="cursor-pointer text-white">Projects</button>
+					<button class="cursor-pointer text-white">Skills</button>
+				</nav>
+
+				<nav class="flex items-center justify-center gap-2 lg:gap-8">
+					<GitHub />
+					<Linkedin />
+					<Instagram />
+				</nav>
+			</div>
+
+			<button
+				class="flex min-h-full cursor-pointer flex-col justify-center gap-2 sm:hidden"
+				on:click={() => (showSidebar = true)}
+			>
+				<div class="h-1 w-10 rounded-xl bg-gray-500" />
+				<div class="h-1 w-10 rounded-xl bg-gray-500" />
+				<div class="h-1 w-10 rounded-xl bg-gray-500" />
+			</button>
+		</header>
+		<SideBar bind:showSidebar />
+	{/if}
 </div>
