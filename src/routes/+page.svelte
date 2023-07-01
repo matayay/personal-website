070994542876d30dcbe600b4e395c6898eb038b4
Matayay Karuna <matayay.karuna@colorado.edu>
2023-07-01 01:03:54 -0600

    uninstall threlte because the enite library is broken

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core';
-	import { spring } from 'svelte/motion';
-	import { degToRad } from 'three/src/math/MathUtils';
-
-	const scale = spring(1);
 </script>
 
 <div class="min-h-screen bg-black" />
