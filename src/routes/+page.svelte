1a282b56351dd435177a411af3c12b66625942ad
Matayay Karuna <matayay.karuna@colorado.edu>
2023-07-03 20:57:12 -0600

    created experience section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import About from './components/About.svelte';
+	import About from './pages/About.svelte';
+	import Experience from './pages/Experience.svelte';
 </script>
 
 <div class="min-h-screen bg-black">
 	<About />
+	<Experience />
 </div>
