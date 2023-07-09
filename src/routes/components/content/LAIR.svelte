a4fb1c4fd475b4f37adc3bb213e06ac047fe3283
Matayay Karuna <matayay.karuna@colorado.edu>
2023-07-08 23:01:11 -0600

    started experience section

---
+++ src/routes/components/content/LAIR.svelte
@@ -0,0 +1,29 @@
+<article class="flex flex-col items-start justify-center gap-4">
+	<div>
+		<div class="flex items-center justify-center gap-2">
+			<h1 class="text-white">Student Software Engineer</h1>
+			<h1 class="text-cyan-500">@</h1>
+			<button
+				on:click={() => window.open('https://culair.weebly.com/')}
+				class="cursor-pointer text-cyan-500 hover:underline"
+			>
+				CU LAIR Lab
+			</button>
+		</div>
+		<h3 class="text-neutral-500">June 2023 - Present</h3>
+	</div>
+	<ul>
+		<li class="text-neutral-400">
+			Fullstack Engineer responsible for developing a website to interface, monitor, and retrieve
+			data from DAQ sites measuring VLF radio waves
+		</li>
+		<li class="text-neutral-400">Made use of Next.js and Tailwind.css to construct frontend</li>
+		<li class="text-neutral-400">
+			Leveraged AWS services including API Gateway, Lambda, and Dynamodb to construct backend
+		</li>
+		<li class="text-neutral-400">
+			Developed an API based on WebSockets to allow bidirectional communication between website and
+			DAQ sites
+		</li>
+	</ul>
+</article>
--- src/routes/components/sections/Experience.svelte
+++ src/routes/components/sections/Experience.svelte
@@ -1,12 +1,69 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import Lair from '../content/LAIR.svelte';
+
+	let index = 0;
+	let styles = [true, false, false];
+
+	const handleClick = (i: number) => {
+		index = i;
+		for (let j = 0; j < styles.length; j++) {
+			styles[j] = false;
+		}
+		styles[i] = true;
+	};
 </script>
 
 <section
-	class="flex min-h-screen flex-col items-center bg-gradient-to-t from-slate-950 pt-20"
+	class="flex min-h-screen flex-col items-center gap-10 bg-gradient-to-t from-slate-950 pt-20"
 	id="experience"
 >
 	<div class="flex w-full flex-col items-center justify-center gap-4">
 		<h2 class="text-center text-2xl text-neutral-300 sm:text-5xl">My Work Experience</h2>
 		<div class="h-1 w-3/4 rounded-3xl bg-gray-400" />
+	</div>
+
+	<div
+		class="flex max-w-full flex-col items-center justify-center rounded-3xl bg-gray-900 px-4 py-10"
+	>
+		<nav class="flex w-full items-center justify-start overflow-x-scroll">
+			<button
+				class={`flex flex-col items-center justify-center gap-2 bg-white pt-2 ${
+					styles[0] ? 'bg-opacity-5' : 'bg-opacity-0'
+				}`}
+				on:click={() => handleClick(0)}
+			>
+				<h3 class={`w-max px-6 ${styles[0] ? 'text-cyan-500' : 'text-neutral-400'}`}>
+					CU LAIR Lab
+				</h3>
+				<div class={`h-0.5 w-full ${styles[0] ? 'bg-cyan-500' : 'bg-slate-600'}`} />
+			</button>
+
+			<button
+				class={`flex flex-col items-center justify-center gap-2 bg-white pt-2 ${
+					styles[1] ? 'bg-opacity-5' : 'bg-opacity-0'
+				}`}
+				on:click={() => handleClick(1)}
+			>
+				<h3 class={`w-max px-6 ${styles[1] ? 'text-cyan-500' : 'text-neutral-400'}`}>Job 2</h3>
+				<div class={`h-0.5 w-full ${styles[1] ? 'bg-cyan-500' : 'bg-slate-600'}`} />
+			</button>
+
+			<button
+				class={`flex flex-col items-center justify-center gap-2 bg-white pt-2 ${
+					styles[2] ? 'bg-opacity-5' : 'bg-opacity-0'
+				}`}
+				on:click={() => handleClick(2)}
+			>
+				<h3 class={`w-max px-6 ${styles[2] ? 'text-cyan-500' : 'text-neutral-400'}`}>Job 1</h3>
+				<div class={`h-0.5 w-full ${styles[2] ? 'bg-cyan-500' : 'bg-slate-600'}`} />
+			</button>
+		</nav>
+
+		{#if index === 0}
+			<Lair />
+		{:else}
+			<h1>Bruh</h1>
+		{/if}
 	</div>
 </section>
