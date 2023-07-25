424d4e637486eda75d223b5e0f7258a13687b23a
Matayay Karuna <matayay.karuna@colorado.edu>
2023-07-24 21:26:22 -0600

    updated readme and added repo link in footer

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # Personal Website
 
-080C13 - black
-050D1A - navy
+**Description:** A personal website for me to outline my experience and skills. Includes links to resume, github, and linkedin.
+
+---
+
+# Preview
+
+![](./static//website.gif)
+
+---
+
+# Built With
+
+- Typescript
+- SvelteKit
+- Tailwind.css
+
+---
+
+# Aknowledgements
+
+**Design Inspired by the following sites**
+
+- [Brittany Chiang](https://brittanychiang.com/)
+- [Simon Walker](https://simonwalker.tech/)
--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import branch from './branch.png';
 	const time = new Date();
 </script>
 
 <footer
-	class="flex h-20 items-center justify-center border-x-0 border-t-[0.1px] border-gray-500 bg-black"
+	class="flex h-20 items-center justify-center gap-4 border-x-0 border-t-[0.1px] border-gray-500 bg-black"
 >
 	<h3 class="text-gray-500">© {time.getFullYear()} Matayay Karuna</h3>
+	<button on:click={() => window.open('https://github.com/matayay/personal-website')}>
+		<img src={branch} alt="Github branch" class="h-8 w-8" />
+	</button>
 </footer>
